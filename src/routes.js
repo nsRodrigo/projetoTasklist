49e11b56531b35c019f59f9bba1f07925357c554
Rodrigo do N Silva <rodrigo.nascim.silva@gmail.com>
2024-05-19 14:27:28 -0300

    Prettier e remoção do readme

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const routes = new Router();
 
 routes.get('/teste', (req, res) => {
-    return res.json({ ok: true });
+  return res.json({ ok: true });
 });
 
 export default routes;
